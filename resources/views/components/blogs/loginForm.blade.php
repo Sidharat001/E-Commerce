0e4d3fdee69d4d47c4aa8d72d7f9982f71b2c876
sidharat001 <dr.sidharat@gmail.com>
2023-03-13 15:50:01 +0530

    login form complete

--- resources/views/components/blogs/loginForm.blade.php
+++ resources/views/components/blogs/loginForm.blade.php
@@ -1,11 +1,10 @@
-<div class="container">
-    <h1>Login</h1>
-    <form>
-        <div class="row">
+<div class="container my-5">
+    <form class="m-auto">
+        <div class="row mb-3">
             <label for="email">Email</label>
             <input type="email" name="email" autocomplete="off" placeholder="email@example.com">
         </div>
-        <div class="row">
+        <div class="row mb-3">
             <label for="password">Password</label>
             <input type="password" name="password">
         </div>
